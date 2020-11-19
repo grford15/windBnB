acab86c22090a9b54d7b1981d1200d27fc209105
Greg <g.rutherford10@gmail.com>
2020-11-19 15:48:57 +0000

    add filter button to nav section

--- src/App.js
+++ src/App.js
@@ -1,55 +1,65 @@
-import React from 'react';
-import axios from 'axios';
-import logoSVG from './logo.svg';
-import PropertyCard from './components/PropertyCard';
-import './App.css';
+import React from "react";
+import axios from "axios";
+import logoSVG from "./logo.svg";
+import PropertyCard from "./components/PropertyCard";
+import { AiOutlineControl } from 'react-icons/ai';
+import "./App.css";
 
 class App extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
-      properties: []
-    }
+      properties: [],
+    };
   }
 
   componentDidMount() {
-    axios.get('./stays.json')
-    .then((res) => {
-      this.setState({
-        properties: res.data
+    axios
+      .get("./stays.json")
+      .then((res) => {
+        this.setState({
+          properties: res.data,
+        });
       })
-    }).catch((err) => {
-      console.log(err);
-    })
+      .catch((err) => {
+        console.log(err);
+      });
   }
 
   render() {
-    const {properties} = this.state;
-    console.log(properties)
+    const { properties } = this.state;
+    console.log(properties);
 
-    return(
+    return (
       <div className="App">
         <nav>
           <img src={logoSVG} alt="wind bnb logo" id="nav-logo" />
-          <input type="text" placeholder="Search" id="nav-search"></input>
+          <div className="filter-search">
+            <button id="filter">
+              <AiOutlineControl />
+              Filter</button>
+            <input type="text" placeholder="Search" id="nav-search"></input>
+          </div>
         </nav>
         <div className="property-container">
           {properties.length ? (
-            properties.map((property, index) => 
-            <PropertyCard title={property.title} 
-            index={index} 
-            key={index} 
-            photo={property.photo} 
-            type={property.type}
-            rating={property.rating}
-            superHost={property.superHost}
-            />)
-          )
-          : <p>Didn't work</p>
-          }
+            properties.map((property, index) => (
+              <PropertyCard
+                title={property.title}
+                index={index}
+                key={index}
+                photo={property.photo}
+                type={property.type}
+                rating={property.rating}
+                superHost={property.superHost}
+              />
+            ))
+          ) : (
+            <p>Didn't work</p>
+          )}
         </div>
       </div>
-    )
+    );
   }
 }
 
